58b235170c438c2812a8543edc4c17f57d598a92
Matt Sr Hunter <mhunsr@gmail.com>
2023-02-21 21:33:34 -0500

    made the landing page need to make the link to home and a button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-
 import PostList from '../components/PostList';
 import PostForm from '../components/PostForm';
-
 import { QUERY_POSTS } from '../utils/queries';
+
+// import Particles from "react-tsparticles";
+// import { loadLinksPreset } from "tsparticles-preset-links";
+
+// function particlesInit(tsParticles) {
+//   console.log("init", tsParticles);
+
+//   loadLinksPreset(tsParticles);
+// }
+
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_POSTS);
@@ -12,14 +20,14 @@ const Home = () => {
 
   return (
     <main>
-      <div className="flex-row justify-center">
+      <div className= "flex-row justify-center container">
         <div
-          className="col-12 col-md-10 mb-3 p-3"
+          className="col-12 col-md-10 mb-3 p-3 container"
           style={{ border: '1px dotted #1a1a1a' }}
         >
           <PostForm />
         </div>
-        <div className="col-12 col-md-8 mb-3">
+        <div className="col-12 col-md-8 mb-3 container">
           {loading ? (
             <div>Loading...</div>
           ) : (
@@ -29,8 +37,24 @@ const Home = () => {
             />
           )}
         </div>
-      </div>
+{/* <Particles
+          options={{
+            preset: "links",
+          }}
+          init={particlesInit}
+        /> */}
+        
+
+
+
+</div>
+
+
+
+
     </main>
+
+
   );
 };
 
